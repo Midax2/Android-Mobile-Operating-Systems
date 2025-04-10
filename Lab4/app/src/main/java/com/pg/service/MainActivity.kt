e393cff010db8648ef2b85b26c3334fb65ff472f
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-04-10 20:50:37 +0200

    feat: Implement Service functionality and UI
    This commit introduces a service that tracks the time passed, a UI to display it, and button to start and stop the service.
    - Implement `LifeTimeService` to track time passed.
    - Implement UI to display the time passed in the `MainActivity`.
    - Implement `ServiceConnection` for service interaction.
    - Implement `ServiceButton` to control the `LifeTimeService`.
    - Implement `Greeting` with updated UI for `ServiceScreen`.

--- Lab4/app/src/main/java/com/pg/service/MainActivity.kt
+++ Lab4/app/src/main/java/com/pg/service/MainActivity.kt
@@ -1,47 +1,92 @@
 package com.pg.service
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pg.service.ui.theme.ServiceTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), LifeTimeService.ServiceCallback {
+    private lateinit var mService: LifeTimeService
+    private var mBound: Boolean = false
+    private var mTimePassed by mutableIntStateOf(0)
+
+    override fun onGetResult(intResult: Int) {
+        mTimePassed = intResult
+    }
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName,
+                                        service: IBinder
+        ) {
+            val binder = service as LifeTimeService.LocalBinder
+            mService = binder.getService()
+            mBound = true
+            mService.obtainCallback(this@MainActivity)
+        }
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+            mTimePassed = 0
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ServiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ServiceScreen(serviceConnection, mTimePassed)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(name: String, modifier: Modifier = Modifier, fontSize: TextUnit = 20.sp) {
+    Column(
         modifier = modifier
-    )
+            .height(200.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Laboratory #4", fontSize = fontSize)
+        Text(text = name, fontSize = fontSize)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ServiceScreen(
+        serviceConnection: ServiceConnection,
+        mTimePassed: Int
+) {
     ServiceTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting("Services")
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "Time passed: ${mTimePassed}s")
+            ServiceButton(serviceConnection)
+        }
     }
 }
